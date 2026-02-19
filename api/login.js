1844595cb56b56a4f23b2da6aeb26fb9078170ff
webvertize-ro <contact@webvertize.ro>
2026-02-19 11:07:56 +0200

    further changes

--- api/login.js
+++ api/login.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
     return res.status(401).json({ error: 'Invalid credentials' });
 
   const valid = await bcrypt.compare(password, adminPass);
+  console.log('valid: ', valid);
 
   if (!valid) return res.status(401).json({ error: 'Invalid credentials' });
 
